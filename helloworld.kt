4aef878597642c0e3faaf827839ca5d9055b2870
Your Name <you@example.com>
2022-10-26 09:58:28 +0200

    erster commit

---
+++ helloworld.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+	val firstArg = args[0].toBoolean()
+	var truemessage: String = "MORITZ STINKT!!!!"
+	var falsemessage: String = "SAMUEL STINKT NICHT!!!!"
+		if (firstArg) {
+			for (i in 1..1000) {
+			println(truemessage)
+			}
+				}
+			else {
+				for (i in 1..1000) {
+					println(falsemessage)
+				}
+			}
+				
+
+}
+
+
+
+
+
+
+
+
