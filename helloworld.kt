70ab162e014f19679a939cafad9520e9f78ca33d
Simon Röcker <simon.roecker@inovex.de>
2022-10-26 10:12:59 +0200

    Changed texts

--- helloworld.kt
+++ helloworld.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 	val firstArg = args[0].toBoolean()
-	var truemessage: String = "MORITZ STINKT!!!!"
-	var falsemessage: String = "SAMUEL STINKT NICHT!!!!"
+	var truemessage: String = "Hier stand nie etwas anstößiges"
+	var falsemessage: String = "Hier auch nicht"
 		if (firstArg) {
 			for (i in 1..1000) {
 			println(truemessage)
